8e7702c85acf780a1d2d660d582a5359acf91bba
Jeremy Le Strat <jeremy.lstrat@gmail.com>
2018-08-08 11:42:54 +0200

    Rails new with frontend and form gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'restaurant/name'
+  get 'restaurant/address'
+  get 'restaurant/category'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
